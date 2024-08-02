2b41242cdebff4e1cc52fd1de5623093a9b00389
Harsh Srivastava <srivastavah240@gmail.com>
2024-08-02 16:37:27 +0530

    search , home page done

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,10 +1,54 @@
 import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
+import { navLinks } from "../../../constants";
+import Link from "next/link";
+import { Collection } from "@/components/shared/Collection";
+import { getAllImages } from "@/lib/actions/image.actions";
 
-export default function Home() {
+export default async function  Home({searchParams}:SearchParamProps) {
+  //for pagination we are getting the page and searchquery
+  const page=Number(searchParams?.page)||1;
+  const searchQuery=(searchParams?.query as string )||" "
+  const images=await getAllImages({page ,searchQuery})
   return (
    <div> 
-    Home Page
+      {/* showing banner */}
+    <section className="home">
+      <h1 className="home-heading">
+        Unleash your creativity 🚀 with ReTouchLab 🔮
+      </h1>
+      {/* all images */}
+      <ul className=" flex-center w-full gap-20">
+        {navLinks.slice(1,5).map((link)=>  
+        <Link  className=" flex-col flex-center  gap-2"
+        href={link.route}
+        key={link.route}
+        >
+          <li className=" flex-center w-fit  rounded-full bg-white p-4 ">
+          <Image
+          src={link.icon}
+          alt="link"
+          width={24}
+          height={24}
+          />
+          </li>
+          <p className="p-14-medium  text-center text-white ">
+            {link.label}
+          </p>
+       
+
+
+        </Link>) }
+      </ul>
+    </section>
+    <section className="sm:mt12">
+      <Collection
+      hasSearch={true}
+      images={images?.data}
+      page={page}
+      totalPages={images?.toalPage}
+      />
+    </section>
     
    </div>
   );
--- src/lib/actions/image.actions.ts
+++ src/lib/actions/image.actions.ts
@@ -1,98 +1,139 @@
-"use server"
+"use server";
 
-import { revalidatePath } from "next/cache"
-import { handleError } from "../utils"
-import { connectToDatabase } from "../database/mongoose"
+import { revalidatePath } from "next/cache";
+import { handleError } from "../utils";
+import { connectToDatabase } from "../database/mongoose";
 import User from "../database/models/user.model";
 import Image from "../database/models/image.model";
 import { redirect } from "next/navigation";
-const populateUser=(query:any)=>
-   query.populate(
-    {
-        path:"author",
-        model:User,
-        select:'_id firstName lastName'
-    }
+import { v2 as cloudinary } from "cloudinary";
+const populateUser = (query: any) =>
+	query.populate({
+		path: "author",
+		model: User,
+		select: "_id firstName lastName",
+	});
 
-   )
-
-
-//Add Image 
-export async function addImage({image , userId , path}:AddImageParams ){
-        try {
-            await connectToDatabase();
-            //get user from User db
-            const author= await User.findById(userId)
-            if(!author)
-            {
-                throw new Error("Author  not found");
-            }
-            const newImage= await Image.create(
-                {
-                ...image ,
-                author:author._id
-                }
-            )
-            revalidatePath(path)
-           return  JSON.parse(JSON.stringify(newImage));
-            
-        } catch (error) {
-            handleError(error)
-        }
-
+//Add Image
+export async function addImage({ image, userId, path }: AddImageParams) {
+	try {
+		await connectToDatabase();
+		//get user from User db
+		const author = await User.findById(userId);
+		if (!author) {
+			throw new Error("Author  not found");
+		}
+		const newImage = await Image.create({
+			...image,
+			author: author._id,
+		});
+		revalidatePath(path);
+		return JSON.parse(JSON.stringify(newImage));
+	} catch (error) {
+		handleError(error);
+	}
 }
 //Update Image
-export async function updateImage({image , userId , path}:UpdateImageParams){
-    try {
-        await connectToDatabase();
-        const imageToUpdate= await Image.findById(image._id)
-        if(!imageToUpdate || imageToUpdate.author.toHexString()!==userId)
-        {
-            throw new Error("Image not found or no access to image ")
-        }
-        const updatedImage= await Image.findByIdAndUpdate(
-            imageToUpdate._id ,
-            image ,
-            {new:true}
-        )
-      
-        revalidatePath(path);
-        return  JSON.parse(JSON.stringify(updatedImage));
+export async function updateImage({ image, userId, path }: UpdateImageParams) {
+	try {
+		await connectToDatabase();
+		const imageToUpdate = await Image.findById(image._id);
+		if (!imageToUpdate || imageToUpdate.author.toHexString() !== userId) {
+			throw new Error("Image not found or no access to image ");
+		}
+		const updatedImage = await Image.findByIdAndUpdate(
+			imageToUpdate._id,
+			image,
+			{ new: true }
+		);
 
-    } catch (error) {
-        handleError(error)
-    }
-
-
-
+		revalidatePath(path);
+		return JSON.parse(JSON.stringify(updatedImage));
+	} catch (error) {
+		handleError(error);
+	}
 }
 //Delete Image
-export async  function deleteImage(imageId:string)
-{
- try {
-      await connectToDatabase();
-      const image=  await Image.findByIdAndDelete(imageId)
- } catch (error) {
-    handleError(error)
- }
- finally
- {
-    redirect("/")
- }
+export async function deleteImage(imageId: string) {
+	try {
+		await connectToDatabase();
+		const image = await Image.findByIdAndDelete(imageId);
+	} catch (error) {
+		handleError(error);
+	} finally {
+		redirect("/");
+	}
 }
 //GET Image
-export async function getImageById(imageId:string){
-    try {
-        await connectToDatabase();
-       const image=await populateUser( Image.findById(imageId))
-       if(!image)
-        {
-            throw new Error("Image not  found")
-        }   
-        return JSON.parse(JSON.stringify(image));
-        
-    } catch (error) {
-        handleError(error)
-    }
-
+export async function getImageById(imageId: string) {
+	try {
+		await connectToDatabase();
+		const image = await populateUser(Image.findById(imageId));
+		if (!image) {
+			throw new Error("Image not  found");
+		}
+		return JSON.parse(JSON.stringify(image));
+	} catch (error) {
+		handleError(error);
+	}
 }
+//GET ALL IMAGES
+export async function getAllImages({
+	limit = 9,
+	page = 1,
+	searchQuery = "",
+}: {
+	limit?: number;
+	page: number;
+	searchQuery?: string;
+}) {
+	try {
+		await connectToDatabase();
+        //configuring cloudinary 
+		cloudinary.config({
+			cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+            api_key:process.env.CLOUDINARY_API_KEY,
+            api_secret:process.env.CLOUDINARY_API_SECRET,
+            secure:true
+		});
+        //initial expression (its folder where we will search for images and get images from their)
+        let expression ="folder=retouchlab"
+        //searching for specific images  based on query that user has entered
+        if(searchQuery)
+        {
+            expression+=`AND ${searchQuery}`
+        }
+        //getting all the resources basedon the exression
+        const {resources}=await cloudinary.search.expression(searchQuery).execute();
+        //after getting all the resources we will get their id . so that we can get that from our database
+        const resourceIds= resources.map((resource:any)=>resource.public_id)
+
+        //querying our own database
+        let query={}
+        if(searchQuery)
+        {
+            query={
+                publicId:{
+                    $in:resourceIds
+                }
+
+            }
+        }
+        const skipAmount=(Number(page)-1)*limit;
+        //getting all the matched images from our db
+        const images=await populateUser(Image.find(query)).sort({updatedAt:-1}).skip(skipAmount).limit(limit)
+        // get image count of images which matches our searched query
+        const totalImages= await Image.find(query).countDocuments();
+        //  counbt of all images store in our db
+        const  savedImage=await Image.find().countDocuments();
+        return {
+            data:JSON.parse(JSON.stringify(images)) ,
+            savedImage ,
+            toalPage:Math.ceil(totalImages/limit)
+
+        }
+	} catch (error) {
+		handleError(error);
+	}
+}
+
